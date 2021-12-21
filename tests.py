68281e79b489fd5df8495b58a4c81d4861c75fe8
Dominika Maziec <dominika.maziec1@gmail.com>
2021-12-21 21:14:00 +0100

    calc fixes

--- tests.py
+++ tests.py
@@ -4,26 +4,31 @@ import filecmp
 from lzw import compress, extract, entropy
 import matplotlib.pyplot as plt
 
+
 def calculate_stopien_kompresji(original_size, compressed_size):
-    result  = original_size/compressed_size
+    result = original_size/compressed_size
     print(f"Stopień kompresji: {result}")
     return result
+
 
 def calculate_procent_kompresji(original_size, compressed_size):
     result = (original_size - compressed_size) / original_size
     print(f"Procent kompresji wynosi: {result}")
     return result
 
+
 def calculate_entropy_zero_condition(file, block_size):
-    result, nb_symbols = entropy(file, block_size, 0)
+    result = entropy(file, block_size, 0)
     print(f"Block size: {block_size}, Entropy: {result}")
 
-    return result, nb_symbols
+    return result
+
 
 def calculate_przeplywnosc(compressed_size, nb_symbols):
     l = compressed_size / nb_symbols
     print("Przeplywnosc ", l)
     return l
+
 
 def calculate_frequencies(file_name):
     content = bytearray(open(file_name, 'rb').read())
@@ -38,6 +43,7 @@ def calculate_frequencies(file_name):
     print(f"File saved in {out}")
     plt.clf()
 
+
 if __name__ == "__main__":
 
     for file in glob.glob("data/*"):
@@ -45,37 +51,32 @@ if __name__ == "__main__":
 
         compressed = f"{file}_compressed.lzw"
         decompressed = f"{file}_decompressed.lzw"
-        file_size = os.path.getsize(file)
+        file_size = os.path.getsize(file) * 8
 
         print(f"Compressing {file} of size {file_size} bytes")
-        compress(file, compressed)
-        compressed_size = os.path.getsize(compressed)
+        _, symbols_count = compress(file, compressed)
+        compressed_size = os.path.getsize(compressed) * 8
+        calculate_przeplywnosc(compressed_size, symbols_count)
 
         print(f"Decompressing {compressed} of size {compressed_size} bytes")
         extract(compressed, decompressed)
-        decompressed_size = os.path.getsize(decompressed)
+        decompressed_size = os.path.getsize(decompressed) * 8
 
         print("Checking if the decompressed file is the same as the input file")
-        assert decompressed_size==file_size
+        assert decompressed_size == file_size
 
         print("Checking if the decompressed file is the same as the original one")
         assert True == filecmp.cmp(file, decompressed)
 
         calculate_stopien_kompresji(file_size, compressed_size)
-        
+
         calculate_procent_kompresji(file_size, compressed_size)
 
         print("Calculate entropy...")
 
-        _, nb_symbols = calculate_entropy_zero_condition(file, 1)
-        calculate_przeplywnosc(compressed_size, nb_symbols)
-
-        _, nb_symbols = calculate_entropy_zero_condition(file, 2)
-        calculate_przeplywnosc(compressed_size, nb_symbols)
-
-        _, nb_symbols = calculate_entropy_zero_condition(file, 3)
-        calculate_przeplywnosc(compressed_size, nb_symbols)
-
+        calculate_entropy_zero_condition(file, 1)
+        calculate_entropy_zero_condition(file, 2)
+        calculate_entropy_zero_condition(file, 3)
 
         print(f"Plotting histogram...")
         calculate_frequencies(file)
